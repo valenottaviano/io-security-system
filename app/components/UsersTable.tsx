d7cd9853364ed3d9e5da5b7384964716bdc0a314
Valentin Ottaviano <valentinottaviano@gmail.com>
2023-11-12 12:33:56 -0300

    fixed usability on mobile

--- app/components/UsersTable.tsx
+++ app/components/UsersTable.tsx
@@ -5,11 +5,11 @@ import React from 'react';
 
 const UsersTable = ({ data }: any) => {
     return (
-        <table className='bg-neutral-800 rounded-md border-separate border-spacing-4 w-[90vw] md:w-[70vw] text-center'>
+        <table className='text-sm md:text-md bg-neutral-800 rounded-md border-separate border-spacing-4 w-[90vw] md:w-[70vw] text-center'>
             <thead>
                 <tr>
                     <th>User Token</th>
-                    <th>Datetime</th>
+                    <th className='hidden md:block'>Datetime</th>
                     <th>Status</th>
                 </tr>
             </thead>
@@ -19,7 +19,7 @@ const UsersTable = ({ data }: any) => {
                     dt.setHours(dt.getHours() - 3)
                     return <tr key={index} className={`${parseInt(status.status) == 1 ? 'text-emerald-400' : "text-white/50"}`}>
                         <td>{status.user_token}</td>
-                        <td>{dt.toISOString().replace('T', ' ').replace('Z', '')}</td>
+                        <td className='hidden md:block'>{dt.toISOString().replace('T', ' ').replace('Z', '')}</td>
                         <td>{parseInt(status.status)}</td>
                     </tr>
                 })}
